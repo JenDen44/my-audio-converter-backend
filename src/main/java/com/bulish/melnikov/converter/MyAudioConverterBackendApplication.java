8a2afa18b1acc721c37795d899677209da99b148
Evgenia Bullis <bulish2015@yandex.ru>
2025-03-20 19:28:02 +0300

    init

---
+++ src/main/java/com/bulish/melnikov/converter/MyAudioConverterBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bulish.melnikov.converter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAudioConverterBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyAudioConverterBackendApplication.class, args);
+	}
+
+}
