e4c90f80a241af9338c3d8deca3b2a6cb6d4839c
Evgenia Bullis <bulish2015@yandex.ru>
2025-03-27 18:55:44 +0300

    add redis as db.

--- src/main/java/com/bulish/melnikov/converter/repository/ConverterRequestRepository.java
+++ src/main/java/com/bulish/melnikov/converter/repository/ConverterRequestRepository.java
@@ -1,9 +1,9 @@
 package com.bulish.melnikov.converter.repository;
 
 import com.bulish.melnikov.converter.model.ConvertRequest;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ConverterRequestRepository extends JpaRepository<ConvertRequest, String> {
+public interface ConverterRequestRepository extends CrudRepository<ConvertRequest, String> {
 }
